a9262bf94acaea23dfe0d4cb3e945115d6ec7afe
sandunsameera <sandunsameera25@gmail.com>
2019-04-05 23:58:34 +0530

    innitial commit

--- tourist_to_townie/lib/main.dart
+++ tourist_to_townie/lib/main.dart
@@ -1,5 +1,58 @@
 import 'package:flutter/material.dart';
+import 'SecondScreen.dart';
 
-void main(){
-
+void main() {
+  runApp(MaterialApp(
+    home: MyPage(),
+  ));
 }
+
+class MyPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) =>
+      Scaffold(
+          appBar: AppBar(
+            title: Text("Welcome"),
+          ),
+          body: Container(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+//            mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  Text(
+                    "Tourist 2 Townie",
+                    textScaleFactor: 3.0,
+                    textAlign: TextAlign.center,
+                  ),
+                  FlatButton(
+                    onPressed: ()  {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => SecondRoute()),);
+                    },
+                    color: Colors.black,
+                    textColor: Colors.white,
+                    padding: EdgeInsets.all(10.0),
+                    child: Column(
+                      children: <Widget>[Text("Sign in as Tourist")],
+                    ),
+                  ),
+                  FlatButton(
+                    onPressed: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (context)=>SecondRoute()),);
+                    },
+                    color: Colors.black,
+                    textColor: Colors.white,
+                    padding: EdgeInsets.all(10.0),
+                    child: Column(
+                      children: <Widget>[
+                        Text("Sign in as Guide"),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ));
+}
+
+
