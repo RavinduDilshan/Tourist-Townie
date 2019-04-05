71ba5efbb41030b2b8fe4f05c5d01ce1a0488636
sandunsameera <sandunsameera25@gmail.com>
2019-04-06 00:45:58 +0530

    Second Screen

--- tourist_to_townie/lib/SecondScreen.dart
+++ tourist_to_townie/lib/SecondScreen.dart
@@ -1,5 +1,46 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MaterialApp(
+    home: Login(),
+  ));
+}
+
+class Login extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+          body: Center(
+        child: Container(
+            padding: EdgeInsets.all(25.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                TextField(
+                  decoration: InputDecoration(hintText: "Email"),
+                ),
+                SizedBox(
+                  height: 15.0,
+                ),
+                TextField(
+                  decoration: InputDecoration(hintText: "Password"),
+                ),
+                RaisedButton(
+                  child: Text("Login"),
+                  color: Colors.lightBlueAccent,
+                  textColor: Colors.black,
+                  onPressed: () {},
+                ),
+                RaisedButton(
+                  child: Text("Dont have account?"),
+                  color: Colors.lightBlueAccent,
+                  textColor: Colors.black,
+                  onPressed: () {},
+                )
+              ],
+            )),
+      ));
+}
+
 class SecondRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -8,7 +49,6 @@ class SecondRoute extends StatelessWidget {
       appBar: AppBar(
         title: Text("Login Page"),
       ),
-      body: Text("Log here"),
     );
   }
 }
--- tourist_to_townie/lib/main.dart
+++ tourist_to_townie/lib/main.dart
@@ -9,50 +9,54 @@ void main() {
 
 class MyPage extends StatelessWidget {
   @override
-  Widget build(BuildContext context) =>
-      Scaffold(
-          appBar: AppBar(
-            title: Text("Welcome"),
-          ),
-          body: Container(
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+  Widget build(BuildContext context) => Scaffold(
+      appBar: AppBar(
+        title: Text("Welcome"),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(5.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
 //            mainAxisSize: MainAxisSize.max,
-                children: <Widget>[
-                  Text(
-                    "Tourist 2 Townie",
-                    textScaleFactor: 3.0,
-                    textAlign: TextAlign.center,
-                  ),
-                  FlatButton(
-                    onPressed: ()  {
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => SecondRoute()),);
-                    },
-                    color: Colors.black,
-                    textColor: Colors.white,
-                    padding: EdgeInsets.all(10.0),
-                    child: Column(
-                      children: <Widget>[Text("Sign in as Tourist")],
-                    ),
-                  ),
-                  FlatButton(
-                    onPressed: () {
-                      Navigator.push(context, MaterialPageRoute(builder: (context)=>SecondRoute()),);
-                    },
-                    color: Colors.black,
-                    textColor: Colors.white,
-                    padding: EdgeInsets.all(10.0),
-                    child: Column(
-                      children: <Widget>[
-                        Text("Sign in as Guide"),
-                      ],
-                    ),
-                  )
-                ],
+            children: <Widget>[
+              Text(
+                "Tourist 2 Townie",
+                textScaleFactor: 3.0,
+                textAlign: TextAlign.center,
               ),
-            ),
-          ));
+              FlatButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Login ()),
+                  );
+                },
+                color: Colors.black,
+                textColor: Colors.white,
+                padding: EdgeInsets.all(10.0),
+                child: Column(
+                  children: <Widget>[Text("Sign in as Tourist")],
+                ),
+              ),
+              FlatButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SecondRoute()),
+                  );
+                },
+                color: Colors.black,
+                textColor: Colors.white,
+                padding: EdgeInsets.all(10.0),
+                child: Column(
+                  children: <Widget>[
+                    Text("Sign in as Guide"),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ));
 }
-
-
