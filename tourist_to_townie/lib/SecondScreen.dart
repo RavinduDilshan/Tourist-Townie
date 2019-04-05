588f355f9c09b43b033870bf901d109b7f57078f
sandunsameera <sandunsameera25@gmail.com>
2019-04-05 23:59:25 +0530

    Second Screen

---
+++ tourist_to_townie/lib/SecondScreen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SecondRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login Page"),
+      ),
+      body: Text("Log here"),
+    );
+  }
+}
